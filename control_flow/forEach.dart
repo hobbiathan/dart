96f8a54df74ca19755ae370b3896db0f380d71ef
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 23:04:50 -0700

    additional for

--- control_flow/forEach.dart
+++ control_flow/forEach.dart
@@ -4,4 +4,9 @@ void main() {
   // Object_array.forEach((item_in_object) => action_to_do_lol)
   test_array.forEach((string) =>
       print("$string")); // IT HAS A FOR EACHEACH ENUMERABLE HAHAFHAHFAOUFHADF
+
+  // Same thing as above
+  for (final string_element in test_array) {
+    print("$string_element");
+  }
 }
