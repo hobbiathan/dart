3c76df1445a594044f4073e11045f7e1bf971070
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 22:24:48 -0700

    Init commit lul

---
+++ hello_world/hello_world.dart
@@ -0,0 +1,7 @@
+
+// Reminds me of C/C++
+
+void main() {
+    print("Hello, World!"); // Semi-colin is required
+}
+
---
+++ variables/variables.dart
@@ -0,0 +1,20 @@
+void main() {
+    // Dart can infer types
+    var name = "Hobbiathan";
+    var year = 2021;
+    var networth = -489.30;
+    var name_array = ["Hobbiathan", "Jamie", "Ruben"];
+    var hash = {
+
+        'key': ['array', 'value'],
+        'key_2': 'string value lul'
+    };
+
+    print(name);
+    print(year);
+    print(networth);
+    print(name_array);
+    print(hash);
+
+
+}
