1bb66405d48f6ab1cc17968b5122c5412acc9477
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-17 23:00:01 -0700

    AHAH FORE EACH THING AIHFAOF

---
+++ control_flow/forEach.dart
@@ -0,0 +1,7 @@
+void main() {
+  var test_array = ["mouse", "hello", "oink", "jamie", "george"];
+
+  // Object_array.forEach((item_in_object) => action_to_do_lol)
+  test_array.forEach((string) =>
+      print("$string")); // IT HAS A FOR EACHEACH ENUMERABLE HAHAFHAHFAOUFHADF
+}
